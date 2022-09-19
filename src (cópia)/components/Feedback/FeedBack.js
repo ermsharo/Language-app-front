c60ab59a6a7b76b4612c76d1d8c86f1e2244074e
emilio <ermsharo@gmail.com>
2022-09-19 11:06:55 -0300

    update our changes

--- src (cópia)/components/Feedback/FeedBack.js
+++
@@ -1,6 +0,0 @@
-import Alert from "@mui/material/Alert";
-
-export default function Feedback({ status, success, display }) {
-  if (!display) return <></>;
-  return <Alert severity={success ? "success" : "error"}>{status}</Alert>;
-}
--- src (cópia)/components/logo/logo.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const LogoBox = styled.div`
-  font-family: "Sedgwick Ave", sans-serif;
-`;
-
-export default function Logo({ color, size }) {
-  return <LogoBox style={{ color: color, fontSize: size }}>Dict App</LogoBox>;
-}
