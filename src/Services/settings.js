fd1626066502801ac63fb8ecc6c8c86ea3e36326
emilio <ermsharo@gmail.com>
2022-09-19 23:48:42 -0300

    get  host function

---
+++ src/Services/settings.js
@@ -0,0 +1,11 @@
+
+
+const devUrl = 'localhost:3000';
+const prodUrl = '';
+
+const prodUrlServer = "https://dictionary-api-app-back.herokuapp.com/"; 
+const devUrlServer = ""
+
+let getBackendUrl = () => {
+
+}
