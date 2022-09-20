9ca0acaf9608ab9366acec202f456da36a0b8a45
emilio <ermsharo@gmail.com>
2022-09-20 01:21:27 -0300

    update our changes

--- src/Services/settings.js
+++ src/Services/settings.js
@@ -1,11 +1,10 @@
+const devUrl = "localhost:3000";
+const prodUrl = "dictionary-api-app-front.herokuapp.com";
 
-
-const devUrl = 'localhost:3000';
-const prodUrl = '';
-
-const prodUrlServer = "https://dictionary-api-app-back.herokuapp.com/"; 
-const devUrlServer = ""
+const prodUrlServer = "https://dictionary-api-app-back.herokuapp.com/";
+const devUrlServer = "http://localhost:5000/";
 
 let getBackendUrl = () => {
-
-}
+  if (window.location.host === prodUrl) return prodUrl;
+  return devUrl;
+};
